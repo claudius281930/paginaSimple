0561d0c87cb0cf3d3a74a80ecb3472c135c1ddd1
Claudio Lima <95236692+claudius281930@users.noreply.github.com>
2023-03-11 21:01:17 -0300

    Update index.js

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -6,25 +6,7 @@ function Home() {
   return (
 
     <div className={styles.containerHome}>
-
-      <section className={styles.banner}>
-
-        <img className={styles.bannerHome}
-          src="/img/home.png"
-          alt="imagem da pagina inicial" />
-      </section>
-
-      <section className={styles.promotion}>
-
-        <img className={styles.promotionImgs}
-           src="/img/carossel/1-preco.png"
-          alt="imagem um com preços promocionais" />
-
-        <img className={styles.promotionImgs}
-           src="/img/carossel/2-preco.png"
-          alt="imagem dois com preços promocionais" />
-
-      </section>
+      <img className={styles.bannerHome}src="/img/home.png"alt="imagem da pagina inicial" />
     </div>
 
   );
